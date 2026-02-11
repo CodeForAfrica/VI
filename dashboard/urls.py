56feb6c19219dbdf8874f3269fc740c2d5f8295b
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-02-11 16:15:28 +0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.overview, name='home'),
-    path('cii/', views.cii, name='cii'),
+    #path('cii/', views.cii, name='cii'),
     path('countries/', views.countries, name='countries'),
     path('authors/', views.authors, name='authors'),
     path('media/', views.media, name='media'),
     path('articles/', views.all_articles, name='all_articles'), 
     path('report/', views.generate_report, name='generate_report'),
     path('chatbot-response/', views.chatbot_response, name='chatbot_response'),
-    path('actors/', views.actors, name='actors'),
+    #path('actors/', views.actors, name='actors'),
 ]
