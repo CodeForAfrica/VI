e2c9f264290b800f6a08e84d77e66a9237479176
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-02-12 12:38:42 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from dashboard import views  # ADD THIS LINE
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -8,7 +9,6 @@ urlpatterns = [
     path('vulnerability-index/', include('dashboard.urls')),  # SEO-friendly URL
     path('vi-tool/', include('dashboard.urls')),  # Another SEO-friendly option
     path('dashboard/', include('dashboard.urls')),  # Dashboard URL
+    path('generate-report/', views.generate_report, name='generate_report'),  # ADD THIS
     path('index/', RedirectView.as_view(pattern_name='overview'), name='index'),  # Redirect
-    path('generate-report/', views.generate_report, name='generate_report'),
-    path('report/', views.generate_report, name='report'),
 ]
