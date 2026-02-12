7a5689bfbb5eec2b10cc7c82d768c1f276d1c2b0
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-02-12 11:10:16 +0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.overview, name='home'),
-    #path('cii/', views.cii, name='cii'),
-    path('countries/', views.countries, name='countries'),
+    path('', views.overview, name='overview'),
+    path('vulnerability-index/', views.overview, name='vulnerability_index'),
+    path('dashboard/', views.overview, name='dashboard_overview'),
+    path('vi/', views.overview, name='vi_tool'),
     path('authors/', views.authors, name='authors'),
     path('media/', views.media, name='media'),
-    path('articles/', views.all_articles, name='all_articles'), 
-    path('report/', views.generate_report, name='generate_report'),
+    path('intents/', views.intents, name='intents'),
+    path('countries/', views.countries, name='countries'),
+    path('all-articles/', views.all_articles, name='all_articles'),
+    path('articles/', views.articles_view, name='articles'),
+    path('generate-report/', views.generate_report, name='generate_report'),
     path('chatbot-response/', views.chatbot_response, name='chatbot_response'),
-    #path('actors/', views.actors, name='actors'),
 ]
