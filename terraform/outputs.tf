5ed595d326630b563ae726418848562277ec1bec
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-02-19 10:24:20 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,13 @@ output "lambda_role_arn" {
   description = "ARN of the Lambda IAM role"
   value       = aws_iam_role.lambda_role.arn
 }
+
+output "lambda_function_arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.mediacloud_ingestion.arn
+}
+
+output "lambda_last_modified" {
+  description = "Last modified timestamp of the Lambda function"
+  value       = aws_lambda_function.mediacloud_ingestion.last_modified
+}
