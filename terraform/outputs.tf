adddbf4417ce7feef772763028ad0a32e27e4de5
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-02-23 16:07:37 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,7 +10,7 @@ output "eventbridge_rule" {
 
 output "lambda_role_arn" {
   description = "ARN of the Lambda IAM role"
-  value       = aws_iam_role.lambda_role.arn
+  value       = var.lambda_role_arn
 }
 
 output "lambda_function_arn" {
