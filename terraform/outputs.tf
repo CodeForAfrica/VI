51a96444b6922e9d986030043bce5add808b378b
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-02-24 12:04:10 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,11 @@
 output "lambda_function_name" {
-  description = "Name of the Lambda function"
-  value       = aws_lambda_function.mediacloud_ingestion.function_name
-}
-
-output "eventbridge_rule" {
-  description = "EventBridge rule name"
-  value       = aws_cloudwatch_event_rule.daily_ingestion.name
-}
-
-output "lambda_role_arn" {
-  description = "ARN of the Lambda IAM role"
-  value       = "lambda_role_arn"
+  value = aws_lambda_function.my_lambda.function_name
 }
 
 output "lambda_function_arn" {
-  description = "ARN of the Lambda function"
-  value       = aws_lambda_function.mediacloud_ingestion.arn
+  value = aws_lambda_function.my_lambda.arn
 }
 
 output "lambda_last_modified" {
-  description = "Last modified timestamp of the Lambda function"
-  value       = aws_lambda_function.mediacloud_ingestion.last_modified
+  value = aws_lambda_function.my_lambda.last_modified
 }
