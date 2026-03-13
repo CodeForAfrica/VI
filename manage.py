1c943bfa14074978d7caaf832cb73b8d47efcb28
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-03-13 14:25:44 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-from django.conf import settings
-from django.core.management.base import BaseCommand
-from django.db.models import Q
 import django
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
