5b1b39a5204c8a269567ee958e89b441cebb34c8
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-03-16 13:51:00 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('generate-report/', views.generate_report, name='generate_report'),
     path('report/', views.generate_report, name='report'),
     path('chatbot-response/', views.chatbot_response, name='chatbot_response'),
+    path('clear-cache/', clear_cache_view, name='clear_cache'),
 ]
