4bbfc846f67b0adb2204ea104b8f36a600015ac8
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-03-19 11:55:00 +0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
     path('dashboard/', views.overview, name='dashboard_overview'),
     path('vi/', views.overview, name='vi_tool'),
     path('authors/', views.authors, name='authors'),
-    path('media-outlets/', views.media, name='media')
+    # CHECK THE COMMA AT THE END OF THE LINE BELOW:
+    path('media-outlets/', views.media, name='media'), 
     path('intents/', views.intents, name='intents'),
     path('countries/', views.countries, name='countries'),
     path('all-articles/', views.all_articles, name='all_articles'),
     path('articles/', views.articles_view, name='articles'),
     path('generate-report/', views.generate_report, name='generate_report'),
-    #path('chatbot-response/', views.chatbot_response, name='chatbot_response'),
     path('chat/', views.chatbot_response, name='chatbot_response'),
-    path('clear-cache/', views.clear_cache_view, name='clear_cache'),
+    path('clear-cache/', views.clear_cache_view, name='clear_cache'), # Good practice to keep a comma here too
 ]
